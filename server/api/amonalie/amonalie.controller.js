f99c8a843b6993271c2f6d140f44c36b0289c569
leolmi <leo.olmi@gmail.com>
2015-02-12 01:43:38 +0100

    ottimizzazioni

--- server/api/amonalie/amonalie.controller.js
+++ server/api/amonalie/amonalie.controller.js
@@ -1,1 +1,32 @@
 
+/**
+ * Created by Leo on 03/02/2015.
+ */
+'use strict';
+
+var _ = require('lodash');
+var u = require('../utilities/util');
+var Amonalie = require('./amonalie.model');
+
+
+// Get list of amonalie
+exports.index = function(req, res) {
+  return u.index(Amonalie, req, res);
+};
+
+// Updates an existing amonalia in the DB.
+exports.update = function(req, res) {
+  return u.update(Amonalie, req, res);
+};
+
+// gestisce le amonalie.
+exports.manage = function(req, res) {
+  //TODO: sposta nello storico
+  //TODO: elimina
+  return u.notfound(res);
+};
+
+// scarica le anomalie da assistant
+exports.milk = function(req, res) {
+  return u.notfound(res);
+};
